df19150cec1e59caca562b28314734f242fbda1b
Steven G. Harms <steven@devbootcamp.com>
2015-07-09 11:05:45 -0400

    Breath: Implement duration tracking

--- app/assets/javascripts/meditation_trainer/_src/controller.js
+++ app/assets/javascripts/meditation_trainer/_src/controller.js
@@ -1,8 +1,31 @@
 MeditationTrainer.Breath = function() {
+  this.startDate = new Date();
+  this.pivotDate = null;
+  this.endDate = null;
 }
 
 MeditationTrainer.Breath.prototype = {
-  finish: function() {
+  pivot: function(endDate) {
+    this.pivotDate = new Date()
+  },
+
+  finish: function(endDate) {
+    this.endDate = new Date()
+  },
+
+  inhaleDuration: function() {
+    if (typeof(this.pivotDate) == "undefined") this.endDate(); // Prevent naughtiness
+    return (this.pivotDate - this.startDate) / 1000;
+  },
+
+  exhaleDuration: function() {
+    if (typeof(this.pivotDate) == "undefined") this.endDate(); // Prevent naughtiness
+    return (this.endDate - this.pivotDate) / 1000;
+  },
+
+  cycleDuration: function() {
+    if (typeof(this.endDate) == "undefined") this.endDate(); // Prevent naughtiness
+    return (this.endDate - this.startDate) / 1000;
   }
 }
 
@@ -34,7 +57,7 @@ MeditationTrainer.Controller.prototype = {
 
     if (!this.isInhaling) {
       console.log('creating breath');
-      this.currentBreath = new MeditationTrainer.Breath(new Date());
+      this.currentBreath = new MeditationTrainer.Breath();
       this.isInhaling = true;
     } else if (this.isInhaling) {
       console.log('ending breath');
