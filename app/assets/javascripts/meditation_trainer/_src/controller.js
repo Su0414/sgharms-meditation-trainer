d1cb48c6052213308f1bc375e48fd507c0a96b48
Steven G. Harms <steven@devbootcamp.com>
2015-07-09 10:54:21 -0400

    Stub out breath tracking
    Get basic console logging of Game creation, breath
    creation, breath ending.

--- app/assets/javascripts/meditation_trainer/_src/controller.js
+++ app/assets/javascripts/meditation_trainer/_src/controller.js
@@ -1,6 +1,59 @@
-MeditationTrainer.Controller = function() {
+MeditationTrainer.Breath = function() {
 }
 
-MeditationTrainer.Controller.prototype.razzle = function() {
-  alert('hi');
+MeditationTrainer.Breath.prototype = {
+  finish: function() {
+  }
+}
+
+MeditationTrainer.Game = function() {
+}
+
+MeditationTrainer.Game.prototype = {
+  addBreath: function() {
+  }
+}
+
+MeditationTrainer.Controller = function() {
+  this.SPACEBAR_KEYCODE = 32;
+
+  this.activeGame = null;
+  this.isInhaling = false;
+
+  this._initializeSpacebarListener();
+}
+
+MeditationTrainer.Controller.prototype = {
+  spacebarFired: function(event) {
+    if (this._noGameActive()) {
+      console.log('creating game');
+      this.activeGame = new MeditationTrainer.Game();
+    }
+
+    if (!this.isInhaling) {
+      console.log('creating breath');
+      this.currentBreath = new MeditationTrainer.Breath(new Date());
+      this.isInhaling = true;
+    } else if (this.isInhaling) {
+      console.log('ending breath');
+      this.currentBreath.finish();
+      this.activeGame.addBreath(this.currentBreath)
+      this.isInhaling = false;
+    }
+  },
+
+  _noGameActive: function() {
+    return !this.activeGame;
+  },
+
+  _initializeGame: function() {
+  },
+
+  _initializeSpacebarListener: function() {
+    $("body").on("keyup", function(jqEvent) {
+      if (jqEvent.keyCode == this.SPACEBAR_KEYCODE) {
+        this.spacebarFired(jqEvent);
+      }
+    }.bind(this));
+  }
 }
