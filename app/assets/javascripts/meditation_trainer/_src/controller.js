db2d114bf98968235047cf597588545d00767da0
Steven G. Harms <steven@devbootcamp.com>
2015-07-09 12:01:00 -0400

    Extract StatsView

--- app/assets/javascripts/meditation_trainer/_src/controller.js
+++ app/assets/javascripts/meditation_trainer/_src/controller.js
@@ -1,15 +1,3 @@
-MeditationTrainer.StatsView = function() {
-}
-
-MeditationTrainer.StatsView.prototype.draw = function(ds) {
-  $('#longest-inhale-duration').text(ds.longestInhale());
-  $('#average-inhale-duration').text(ds.averageInhale());
-  $('#longest-exhale-duration').text(ds.longestExhale());
-  $('#average-exhale-duration').text(ds.averageExhale());
-  $('#longest-cycle-duration').text(ds.longestCycle());
-  $('#average-cycle-duration').text(ds.averageCycle());
-}
-
 MeditationTrainer.Controller = function(totalBreaths, statsView) {
   this.SPACEBAR_KEYCODE = 32;
   this.maxBreathCount = typeof(totalBreaths) == "undefined" ? 3 : 0
