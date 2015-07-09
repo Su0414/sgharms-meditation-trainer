7739e66289f8d9fb0145a4c959e42a0f48c890bb
Steven G. Harms <steven@devbootcamp.com>
2015-07-09 11:59:30 -0400

    Integrate StatsView into Controller

--- app/assets/javascripts/meditation_trainer/_src/controller.js
+++ app/assets/javascripts/meditation_trainer/_src/controller.js
@@ -10,12 +10,15 @@ MeditationTrainer.StatsView.prototype.draw = function(ds) {
   $('#average-cycle-duration').text(ds.averageCycle());
 }
 
+MeditationTrainer.Controller = function(totalBreaths, statsView) {
   this.SPACEBAR_KEYCODE = 32;
   this.maxBreathCount = typeof(totalBreaths) == "undefined" ? 3 : 0
 
   this.activeGame = null;
   this.isInhaling = false;
   this.breathCount = 0;
+
+  this.statsView = statsView;
 
   this._initializeSpacebarListener();
 }
@@ -36,28 +39,30 @@ MeditationTrainer.Controller.prototype = {
       } else {
         console.log('ending breath');
         this.currentBreath.finish();
+        this.breathCount++;
         this.isExhaling = false;
+        this._updateStatsView();
+        if (this.breathCount == this.maxBreathCount) {
+          console.log('game over');
+        }
       }
 
-      if (this.breathCount == this.maxBreathCount) {
-        console.log('game over');
-      }
     } else if (this.isInhaling) {
       console.log('pivoting breath');
       this.currentBreath.pivot();
       this.activeGame.addBreath(this.currentBreath)
       this.isInhaling = false;
       this.isExhaling = true;
-      this.breathCount++;
     }
 
   },
 
-  _noGameActive: function() {
-    return !this.activeGame;
+  _updateStatsView: function() {
+    this.statsView.draw(this.activeGame);
   },
 
-  _initializeGame: function() {
+  _noGameActive: function() {
+    return !this.activeGame;
   },
 
   _initializeSpacebarListener: function() {
