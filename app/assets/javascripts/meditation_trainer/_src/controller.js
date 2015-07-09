317ffa2bd19cd9213860441a069c401447ad15e0
Steven G. Harms <steven@devbootcamp.com>
2015-07-09 11:57:58 -0400

    Add MeditationTrainer.StatsView

--- app/assets/javascripts/meditation_trainer/_src/controller.js
+++ app/assets/javascripts/meditation_trainer/_src/controller.js
@@ -1,4 +1,15 @@
-MeditationTrainer.Controller = function(totalBreaths) {
+MeditationTrainer.StatsView = function() {
+}
+
+MeditationTrainer.StatsView.prototype.draw = function(ds) {
+  $('#longest-inhale-duration').text(ds.longestInhale());
+  $('#average-inhale-duration').text(ds.averageInhale());
+  $('#longest-exhale-duration').text(ds.longestExhale());
+  $('#average-exhale-duration').text(ds.averageExhale());
+  $('#longest-cycle-duration').text(ds.longestCycle());
+  $('#average-cycle-duration').text(ds.averageCycle());
+}
+
   this.SPACEBAR_KEYCODE = 32;
   this.maxBreathCount = typeof(totalBreaths) == "undefined" ? 3 : 0
 
